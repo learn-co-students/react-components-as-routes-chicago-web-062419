6c8711ea661d54c10052ed8051a6c195c1857472
github username <github email address>
2019-09-03 16:34:21 -0500

    done

---
+++ src/About.js
@@ -0,0 +1,20 @@
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+import React, { Component } from "react";
+class About extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>This is my about component!</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
